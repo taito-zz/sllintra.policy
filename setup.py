40f8b9407bbc908dfcc358f9785e845906a8eab7
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-01-23 14:01:50 +0200

    release 0.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sllintra.policy',
-    version='0.0',
+    version='0.0.1',
     description="Turns Plone site into SLL intra site.",
     long_description=open("README.rst").read(),
     classifiers=[
